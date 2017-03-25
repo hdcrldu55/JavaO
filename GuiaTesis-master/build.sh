02dc71032af1c28dbaf198584405eb1accc19838
Hugo Cárdenas <hdcrldu55@gmail.com>
2017-03-25 11:27:01 -0500

    p

--- GuiaTesis-master/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mvn clean install
-rhc scp rs upload target/guiaquito.war wildfly/standalone/deployments/
--- GuiaTesis-master/src/main/java/ec/edu/epn/guiaquito/config/JaxRsActivator.java
+++
@@ -1,8 +0,0 @@
-package ec.edu.epn.guiaquito.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class JaxRsActivator extends Application {
-}
